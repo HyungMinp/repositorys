ed4e6fbaa6ff97ac5d34b3684cfd534791008dfc
Park <1oommoo1@naver.com>
2025-09-14 19:12:30 +0900

    커밋 테스트

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-//asd ydy test1 test4 test5 tes6 test7 test8 test9 test10 test11 test13 zzz
+//asd ydy test1 test4 test5 tes6 test7 test8 test9 test10 test11 test13 zzz yrdy2
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
