d17de3aae15820eccf6ef0dfd084440319506cd0
Park <1oommoo1@naver.com>
2025-09-14 18:43:12 +0900

    sssssssssssssssssssss

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-//asd ydy test1 test4 test5 tes6 test7 test8 test9 test10
+//asd ydy test1 test4 test5 tes6 test7 test8 test9 test10 test11
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
